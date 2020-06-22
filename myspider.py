d3dc3e52de5ab2fafd2592e5d530058679537695
alenakuz <alenalexkuz@gmail.com>
2020-06-22 14:19:18 +0300

    init

---
+++ myspider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://blog.scrapinghub.com']
+
+    def parse(self, response):
+        print("parse"
+              "\n\n\n\n")
+        print(response.css(), "-response\n")
+        for title in response.css('.post-header>h2'):
+            yield {'title': title.css('a ::text').get()}
+
+        for next_page in response.css('a.next-posts-link'):
+            yield response.follow(next_page, self.parse)
